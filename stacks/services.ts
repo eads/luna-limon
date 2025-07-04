c17c1afdfec1e23daed5a27106f5ea71d8525dbe
David Eads <davideads@gmail.com>
2025-07-04 16:57:59 -0500

    Restore other lambas.

--- stacks/services.ts
+++ stacks/services.ts
@@ -1,5 +1,7 @@
 // import * as sst from "sst";
 export function Services() {
+  const stage = $app.stage;
+  
   const resizer = new sst.aws.Function("ImageResizerFn", {
     handler: "packages/services/image-resizer.handler",
     nodejs: { install: ["sharp"] },
@@ -19,5 +21,39 @@ export function Services() {
     ]
   });
 
-  return { resizer };
+
+  // SQS FIFO queue for debouncing build triggers
+  const buildQueue = new sst.aws.Queue('BuildQueue', {
+    fifo: true,
+    contentBasedDeduplication: true,
+    queueName: 'luna-limon-builds.fifo',
+    delay: '30 seconds',
+    visibilityTimeout: '900 seconds',
+  });    
+  
+  // Attach invoker Lambda as a subscriber of the queue
+  buildQueue.subscribe({
+    handler: 'packages/services/airtable-build-invoker.handler',
+    functionName: 'BuildQueueConsumerFunction',
+    nodejs: { install: ['@aws-sdk/client-codebuild'] },
+    environment: {
+      CODEBUILD_PROJECT: process.env.CODEBUILD_PROJECT ?? '',
+      SST_STAGE: stage,
+    },
+  });
+
+  // Airtable Webhook function: enqueues messages into SQS
+  const airtableWebhook = new sst.aws.Function('AirtableWebhookFn', {
+    handler: 'packages/services/airtable-webhook.handler',
+    nodejs: { install: ['@aws-sdk/client-sqs'] },
+    url: true,
+    environment: {
+      WAIT_BEFORE_BUILD: process.env.WAIT_BEFORE_BUILD ?? '30000',
+      BUILD_QUEUE_URL: buildQueue.url,
+      SST_STAGE: stage,
+    },
+    link: [buildQueue],
+  });
+
+  return { resizer, buildQueue, airtableWebhook };
 }
